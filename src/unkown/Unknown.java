ca065971857c30877fb84ac62125a621efa8a0d3
somoruyi <somoruyi@icloud.com>
2025-03-17 21:17:43 -0500

    updated code

---
+++ src/unkown/Unknown.java
@@ -0,0 +1,14 @@
+package src.unkown;
+
+/**
+ */
+public class Unknown {
+
+    /**
+     *  997. Find the Town Judge <br>
+     *  https://leetcode.com/problems/find-the-town-judge/description/
+     */
+    public static void dummy(){
+
+    }
+}
